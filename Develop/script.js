9b197153ef2fcfbec880fae26d959ff56bb99c52
Molly Salmonsen <mollysalmonsen@Mollys-MacBook-Pro-2.local>
2022-08-13 19:41:01 -0400

    Working on JavaScript & some HTML Updates need to display the weather next

--- Develop/script.js
+++ Develop/script.js
@@ -1,48 +1,67 @@
-var results = $('#result-content');
-var resultTextEl = document.querySelector('#result-text');
-var resultContentEl = document.querySelector('#result-content');
-var searchFormEl = document.querySelector('#search-form');
+//Variables from Left Hand side
+var searchFormEl = $('#search-form');
+var searchInputEl = $('#cityInput');
+var searchListEl = $('.list-group');
+
+//Variables from Right Hand side 
+var currentWeatherEl = $('#currentWeatherEl');
+var currentCity = $('#currentCity');
+var currentDate = $('#currentDate').text(moment().format("M/D/YYYY"));
+var currentWeatherIcon = $('#currentWeatherIcon');
+var cTemp = $('#cTemp');
+var cHum = $('#cHum');
+var cWind = $('#cWind');
+var cWind = $('#cWind');
+var cWind = $('#cWind');
+var lookAheadEl = $('lookAhead');
+
+//API Key
 const apiKey = '64e8e895c4d71cf5e75c9837e6e88c15';
 
-function getParams() {
-    var searchParamsArr = document.location.search.split('&');
-
-    var query = searchParamsArr[0].split('=').pop();
-    var format = searchParamsArr[1].split('=').pop();
-
-    searchAPI(query, format);
-}
-
-var getCurrentWeather = function(cityName) {
-    var apiURL = 'https://api.openweathermap.org/data/2.5/weather?q=' + cityName + '&appid=' + apiKey;
-
-    fetch(apiURL).then(function(response){
-        if(response.ok) {
-            
-        }
-    })
-
-}
-
-
-
-
-
-
-
-
-// If user doesn't enter Location - Console Log will error
+//Take user input, City Name & Send cityName to get coordinates
 function handleSearchFormSubmit(event) {
     event.preventDefault();
+    let cityName = searchInputEl.value.trim();
 
-    var searchInputVal = document.querySelector('#search-input').value;
-
-    if(!searchInputVal) {
-        console.error('You need a search input value!');
+    if (cityName) {
+        getCoords(cityName);
+    } else {
+        console.log("Enter valid City Name!");
+        alert("Enter valid City Name!");
         return;
     }
 }
 
-searchFormEl.addEventListener('submit', handleSearchFormSubmit);
+//Take the city Name from above & convert to coordinates
+var getCoords = function(cityName) {
+    var apiURL = 'https://api.openweathermap.org/data/2.5/weather?q=' + cityName + '&units=imperial&appid=' + apiKey;
 
+    fetch(apiURL).then(function (res){
+        return res.json();
+    }).then(function(data) {
+        let lat = data.coord.lat;
+        let long = data.coord.lon;
+        getWeather(lat,long);
+    })
+    .catch(function(error){
+        console.error(error);
+        return;
+    })
+}
 
+//From the above coordinates go get the Weather
+var getWeather = function(lat, long) {
+    let apiURL = "https://api.openweathermap.org/data/2.5/weather?lat="+ lat+ "&lon=" + long + "&appid=" + apiKey + "&units=imperial"
+    //go to Weather API with Coordinates of the City
+    fetch(apiURL).then(function(res){
+        if(res.ok){
+            return res.json();
+        } else {
+            alert("Enter a valid City");
+        }
+        //If the results are valid go display the weather of the city
+    }) .then(function(data){
+        displayWeather(data);
+        displayLookAhead(data);
+    })
+}
